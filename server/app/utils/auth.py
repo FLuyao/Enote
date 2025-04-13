f6825ea71669b8938a197974a354ab1d4e5e31c4
QingyiLi <2290140253@qq.com>
2025-04-13 22:05:41 +0800

    修改会话管理功能

--- server/app/utils/auth.py
+++ server/app/utils/auth.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import jwt
 from flask import current_app
 
+# 生成token
 def generate_token(username):
     payload = {
         'username': username,
@@ -10,11 +11,13 @@ def generate_token(username):
     secret_key = current_app.config['JWT_SECRET_KEY']
     token = jwt.encode(payload, secret_key, algorithm='HS256')
     return token
-
+# 解析token
 def decode_token(token):
     try:
         secret_key = current_app.config['JWT_SECRET_KEY']
         payload = jwt.decode(token, secret_key, algorithms=['HS256'])
         return payload
     except jwt.ExpiredSignatureError:
-        return None
+        return {'error': '当前登录状态已过期'}
+    except jwt.InvalidTokenError:
+        return {'error': '当前令牌无效'}
