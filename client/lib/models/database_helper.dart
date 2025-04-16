d602a96dedb86a53aa0ffd6220f72689e2ac412c
FLuyao <2388720311@qq.com>
2025-04-16 12:06:25 +0800

    Update database_helper.dart

--- client/lib/models/database_helper.dart
+++ client/lib/models/database_helper.dart
@@ -2,6 +2,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'user.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
@@ -18,9 +19,76 @@ class DatabaseHelper {
     return _db!;
   }
 
+  Future<void> insertUser(User user) async {
+    final dbClient = await db;
+    await dbClient.insert(
+      'users',
+      user.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<bool> resetLocalPassword(String username, String newPassword) async {
+    final dbClient = await db;
+
+    // 检查用户是否存在
+    final result = await dbClient.query(
+      'users',
+      where: 'username = ?',
+      whereArgs: [username],
+    );
+
+    if (result.isEmpty) {
+      return false; // 用户不存在
+    }
+
+    // 更新密码
+    await dbClient.update(
+      'users',
+      {'password': newPassword},
+      where: 'username = ?',
+      whereArgs: [username],
+    );
+
+    return true; // 修改成功
+  }
+
+  Future<void> updateSyncEnabled(String username, bool enabled) async {
+    final dbClient = await db;
+    await dbClient.update(
+      'users',
+      {
+        'SyncEnabled': enabled ? 1 : 0,
+      },
+      where: 'username = ?',
+      whereArgs: [username],
+    );
+  }
+
+  // 修改当前用户的昵称和密码
+  Future<void> updateUsernameAndPassword({
+    required String currentUsername,
+    required String newUsername,
+    required String newPassword,
+  }) async {
+    final dbClient = await db;
+
+    await dbClient.update(
+      'users',
+      {
+        'username': newUsername,
+        'password': newPassword,
+      },
+      where: 'username = ?',
+      whereArgs: [currentUsername],
+    );
+  }
+
+
   Future<Database> _initDb() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, "score_app.db");
+     await deleteDatabase(path);
 
     return await openDatabase(
       path,
@@ -35,6 +103,17 @@ class DatabaseHelper {
   }
 
   Future _onCreate(Database db, int version) async {
+    // ✅ 新增用户信息表
+    await db.execute('''
+ CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId TEXT,
+  username TEXT,
+  password TEXT,
+  SyncEnabled INTEGER
+)
+''');
+
     await db.execute('''
     CREATE TABLE Score (
       Scoreid TEXT PRIMARY KEY,
